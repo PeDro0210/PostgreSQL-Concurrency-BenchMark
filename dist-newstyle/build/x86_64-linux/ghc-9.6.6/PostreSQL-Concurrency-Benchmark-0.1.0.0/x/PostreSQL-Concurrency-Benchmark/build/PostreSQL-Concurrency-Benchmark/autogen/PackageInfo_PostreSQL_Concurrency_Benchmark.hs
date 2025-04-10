7b6ae5b54ee7d6f4766bbb6cd69ed387a42cbcbc
PeDro0210 <pedroruben0210@gmail.com>
2025-04-10 17:21:10 -0600

    initial commit

---
+++ dist-newstyle/build/x86_64-linux/ghc-9.6.6/PostreSQL-Concurrency-Benchmark-0.1.0.0/x/PostreSQL-Concurrency-Benchmark/build/PostreSQL-Concurrency-Benchmark/autogen/PackageInfo_PostreSQL_Concurrency_Benchmark.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module PackageInfo_PostreSQL_Concurrency_Benchmark (
+    name,
+    version,
+    synopsis,
+    copyright,
+    homepage,
+  ) where
+
+import Data.Version (Version(..))
+import Prelude
+
+name :: String
+name = "PostreSQL_Concurrency_Benchmark"
+version :: Version
+version = Version [0,1,0,0] []
+
+synopsis :: String
+synopsis = ""
+copyright :: String
+copyright = ""
+homepage :: String
+homepage = ""
