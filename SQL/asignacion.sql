5a69324ed44a257bdb636e3cf486d55bd7397892
Brariv <riv23088@uvg.edu.gt>
2025-04-11 23:04:37 -0600

    Update asignacion.sql

--- SQL/asignacion.sql
+++ SQL/asignacion.sql
@@ -1,6 +1,4 @@
-SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
-
-BEGIN TRANSACTION;
+BEGIN TRANSACTION ISOLATION LEVEL READ COMMITTED;
 
 INSERT INTO entradas (cliente_id, evento_id, area_id, fila_id, asiento_id, precio, cantidad, estado)
 VALUES (15, 5, 1, 1, 70, 150.00, 1, 'reservada');
@@ -8,5 +6,26 @@ UPDATE asiento
 SET disponible = FALSE
 WHERE id = 70;
 
-COMMIT TRANSACTION;
+COMMIT;
 
+
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+
+INSERT INTO entradas (cliente_id, evento_id, area_id, fila_id, asiento_id, precio, cantidad, estado)
+VALUES (15, 5, 1, 1, 70, 150.00, 1, 'reservada');
+UPDATE asiento
+SET disponible = FALSE
+WHERE id = 70;
+
+COMMIT;
+
+
+BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
+INSERT INTO entradas (cliente_id, evento_id, area_id, fila_id, asiento_id, precio, cantidad, estado)
+VALUES (15, 5, 1, 1, 70, 150.00, 1, 'reservada');
+UPDATE asiento
+SET disponible = FALSE
+WHERE id = 70;
+
+COMMIT;
