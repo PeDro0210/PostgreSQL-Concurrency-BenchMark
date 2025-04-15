76d574eaaf04e8fd4640bd764332bfdbe104e315
Pedro Avila <pedroavila@Pedros-Laptop.local>
2025-04-15 08:20:45 -0600

    fixed pool implementaiton

--- app/SqlHandler/QueryRunner.hs
+++ app/SqlHandler/QueryRunner.hs
@@ -4,60 +4,33 @@
 module SqlHandler.QueryRunner where
 
 import Codec.Binary.UTF8.Generic as B
-import Control.Concurrent (MVar, threadDelay, withMVar)
-import Control.Exception (bracket)
-import Control.Monad.IO.Class (liftIO)
-import Data.Ratio ((%))
-import Data.Text as Dt
 import Data.Time
-import Hasql.Connection as Connection
-import Hasql.Connection.Setting as Connection
-import Hasql.Connection.Setting.Connection as Connection
-import qualified Hasql.Connection.Setting.Connection as ConnSetting
 import Hasql.Decoders as Decoders
 import Hasql.Encoders as Encoders
-import qualified Hasql.Pool as Pool
-import qualified Hasql.Pool.Config as Pool
+import qualified Hasql.Pool as TPool
 import Hasql.Session as SessionInstance
 import Hasql.Statement (Statement (..))
-import Text.ParserCombinators.ReadP (satisfy)
 import Text.Printf (printf)
 
-dbConnectionHandler :: Int -> [String] -> String -> IO ()
-dbConnectionHandler thread_n queries connectionString = do
+dbConnectionHandler :: [String] -> TPool.Pool  -> IO ()
+dbConnectionHandler queries pool = do
   -- Text Parsing for satisfying the pool.settings
-  let parsedConnection = Dt.pack connectionString
+   start <- getCurrentTime
 
-  -- Fucking stupid
-  -- Ik is kinda dumb reinstanciate it the pool everytime, but it works also
-  let poolSettings =
-        Pool.settings -- Fucking stupid documentation of the pool
-          [ Pool.size thread_n,
-            Pool.acquisitionTimeout 10,
-            Pool.agingTimeout 1800,
-            Pool.idlenessTimeout 1800,
-            Pool.staticConnectionSettings
-              [Connection.connection (ConnSetting.string parsedConnection)]
-          ]
+   result <- TPool.use pool (queryCallSession queries)
 
-  -- passing each pool and running the statement
-  bracket (Pool.acquire poolSettings) Pool.release $ \pool -> do
-    start <- getCurrentTime
+   stop <- getCurrentTime
 
-    result <- Pool.use pool (queryCallSession queries)
+   -- ignore this fucking whole parsing process
+   let timePassed = diffUTCTime stop start
+   let timeParsed = nominalDiffTimeToSeconds timePassed
+   let timeReal = toRational timeParsed
 
-    stop <- getCurrentTime
-
-    -- ignore this fucking whole parsing process
-    let timePassed = diffUTCTime stop start
-    let timeParsed = nominalDiffTimeToSeconds timePassed
-    let timeReal = toRational timeParsed
-
-    case result of
-      Left err ->
-        printf "-> %.5f | Failed |\n" (fromRational timeReal :: Double)
-      Right _ ->
-        printf "-> %.5f | Succed |\n" (fromRational timeReal :: Double)
+   case result of
+     Left err -> do
+       printf "-> %.5f | Failed |\n" (fromRational timeReal :: Double)
+     Right _ ->
+       printf "-> %.5f | Succed |\n" (fromRational timeReal :: Double)
 
 queryCallSession :: [String] -> SessionInstance.Session [()]
 queryCallSession queries = do
--- app/SqlHandler/ThreadsRunner.hs
+++ app/SqlHandler/ThreadsRunner.hs
@@ -4,13 +4,19 @@ import Control.Concurrent (forkIO)
 import Control.Concurrent.MVar
 import Control.Monad (forM_, replicateM)
 import SqlHandler.QueryRunner (dbConnectionHandler)
+import Control.Exception (bracket)
+import Data.Text as Dt
+import Hasql.Connection.Setting as Connection
+import qualified Hasql.Connection.Setting.Connection as ConnSetting
+import qualified Hasql.Pool as TPool
+import qualified Hasql.Pool.Config as CPool
 
 forkThreads :: Int -> IO () -> IO ()
 forkThreads n action = do
   doneVars <- replicateM n newEmptyMVar -- showing that is finish
   -- Idunno why using the forM_ rather than forM, but it seems like the forM can't manage []
   putStrLn "\n"
-  forM_ (zip [1 .. n] doneVars) $ \(i, doneVar) -> do
+  forM_ (Prelude.zip [1 .. n] doneVars) $ \(i, doneVar) -> do
     forkIO $ do
       action
       putMVar doneVar () -- signal completion
@@ -19,11 +25,20 @@ forkThreads n action = do
 
 threadRunner :: [[String]] -> Int -> String -> IO ()
 threadRunner queries n uri = do
-  mutex <- newMVar ()
-  mapM_
-    ( \query ->
-        forkThreads
-          n
-          (dbConnectionHandler n query uri)
+  let parsedConnection = Dt.pack uri
+  -- Fucking stupid
+  -- Ik is kinda dumb reinstanciate it the pool everytime, but it works also
+  let poolSettings =
+        CPool.settings -- Fucking stupid documentation of the pool
+          [ CPool.size n,
+            CPool.acquisitionTimeout 10,
+            CPool.agingTimeout 1800,
+            CPool.idlenessTimeout 1800,
+            CPool.staticConnectionSettings -- not using the dynamic, cause' there's just one uri 
+              [Connection.connection (ConnSetting.string parsedConnection)]
+          ]
+
+  -- passing each pool and running the statement
+  bracket (TPool.acquire poolSettings) TPool.release ( \pool -> do
+      mapM_ ( \query -> forkThreads n (dbConnectionHandler  query pool) )queries
     )
-    queries
